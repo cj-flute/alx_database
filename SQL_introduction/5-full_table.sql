63cb853bc4d14a59a4d76c075e0eb6ccb2af58e2
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-01 02:02:37 +0100

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,11 @@
 -- A script that print the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- CREATE TABLE IF NOT EXISTS first_table(
 
-USE hbtn_0c_0;
+--     id INT,
 
-CREATE TABLE
-    IF NOT EXISTS first_table(id INT, name VARCHAR(256));
+--     name VARCHAR(256)
 
-DESCRIBE first_table;
+-- );
+
+SHOW CREATE TABLE fisrt_table;
