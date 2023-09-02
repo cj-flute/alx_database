0fbd02990c1a28c93094bec0925dfb130ab1bbf5
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-02 16:41:07 +0100

    My privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- A script that list all privieges of the MySQL users user_0d_2 on your server (in localhost).ADD
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
