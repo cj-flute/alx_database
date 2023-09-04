6e38a91376078055df39378eb9c8f34a599e53f2
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-04 14:06:15 +0100

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,9 @@
 -- Write a script that creates the MySQL server user user_0d_1
 
-CREATE USER IF NOT EXISTS 'user_01'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
-GRANT ALL ON *.* TO 'user_01'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+
+SET PASSWORD FOR 'USER_0d_1'@'localhost' = PASSWORD('1111');
+
+FLUSH PRIVILEGES;
