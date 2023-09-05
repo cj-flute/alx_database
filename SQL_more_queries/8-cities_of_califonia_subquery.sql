0587afca38091792eda5d2630c40372d4800afbd
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-05 10:18:40 +0100

    cities of califonia

--- SQL_more_queries/8-cities_of_califonia_subquery.sql
+++ SQL_more_queries/8-cities_of_califonia_subquery.sql
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 -- A script that list all the cities of califonia that can be found in the database hbtn_0d_usa
 
-USE hbtn_0d_usa;
-
 INSERT INTO states (id, name) VALUES (1, 'Califonia');
 
-CREATE TABLE
-    IF NOT EXISTS cities(
-        id INT AUTO_INCREMENT,
-        state_id INT states.id FOREIGN KEY,
-        name VARCHAR(256) NOT NULL
-    );
+-- CREATE TABLE
+
+--     IF NOT EXISTS cities(
+
+--         id INT AUTO_INCREMENT,
+
+--         state_id INT states.id FOREIGN KEY,
+
+--         name VARCHAR(256) NOT NULL
+
+--     );
 
 SELECT cities.name
 FROM cities
