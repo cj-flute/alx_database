19aa1f7e57afd8a2260751b015c61d66c64cca67
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-05 08:25:02 +0100

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS user_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 GRANT SELECT ON 'user_0d_2'.* TO 'user_0d_2'@'localhost';
 
